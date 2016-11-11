0c112d3b129b6bde1adb61e83f9880f606554e94
Christoper0710 <g.gorillagorilla@gmail.com>
2016-11-11 10:08:38 +0900

    11/11 10:07 commit by chris
    花の色に分岐を追加
    モデル追加

--- Misoten/Assets/Scenes/Test/Script/ObjectManager.cs
+++ Misoten/Assets/Scenes/Test/Script/ObjectManager.cs
@@ -3,16 +3,25 @@ using System.Collections;
 
 public class ObjectManager : MonoBehaviour {
 
-    public static GameObject[] objectList = new GameObject[1000];
+    public static GameObject[] objectList = new GameObject[2000];
+    public static Renderer[] rendererList= new Renderer[2000];
+    public static GM_MathFlowerParam.EFlowerColor[] colorList = new GM_MathFlowerParam.EFlowerColor[2000];
     private static int Id = 0;
     private static GameObject prefab;
-    public Material[] mMaterialsA;
-    public static Material[] gMaterialsA;
-
+    public Material[] mMaterialsRed;
+    public Material[] mMaterialsBlue;
+    public Material[] mMaterialsGreen;
+    public static Material[] gMaterialsRed;
+    public static Material[] gMaterialsBlue;
+    public static Material[] gMaterialsGreen;
+    
 	// Use this for initialization
 	void Start () {
         prefab = (GameObject)Resources.Load("Prefabs/Flower");
-        gMaterialsA = mMaterialsA;
+        gMaterialsRed = mMaterialsRed;
+        gMaterialsBlue = mMaterialsBlue;
+        gMaterialsGreen = mMaterialsGreen;
+        //gMaterials = mMaterials;
         Id = 0;
 	}
 	
@@ -29,9 +38,20 @@ public class ObjectManager : MonoBehaviour {
         GM_MathFlowerParam.EFlowerType _type,       // オブジェクトタイプ
         GM_MathFlowerParam.EFlowerColor _color)     // 花の色(花以外はNONE)
     {
-        // プレハブインスタンス
-        Vector3 pos = new Vector3(_position.x, _position.y + 0.5f, _position.z);
-        objectList[Id] = Instantiate(prefab, pos, Quaternion.Euler(60, 180, 0)) as GameObject;
+       
+        switch (_type)
+        {
+            // 花
+            case GM_MathFlowerParam.EFlowerType.Flower1:
+                // プレハブインスタンス
+                Vector3 pos = new Vector3(_position.x, _position.y + 0.5f, _position.z);
+                objectList[Id] = Instantiate(prefab, pos, Quaternion.Euler(60, 180, 0)) as GameObject;
+                rendererList[Id] = objectList[Id].GetComponent<Renderer>();
+                colorList[Id] = _color;
+                break;
+        }
+
+       
         
         return Id++;
     }
@@ -41,10 +61,26 @@ public class ObjectManager : MonoBehaviour {
     public static void LevelUp( int no, int level, GM_MathFlowerParam.EFlowerType _type )
     {
         if (objectList[no] != null){
+            // オブジェクトタイプを判定
             switch (_type)
             {
+                // 花
                 case GM_MathFlowerParam.EFlowerType.Flower1:
-                    objectList[no].GetComponent<Renderer>().material = gMaterialsA[level-1];
+                    //　色分け
+                    switch (colorList[no])
+                    {
+                            // 赤
+                        case GM_MathFlowerParam.EFlowerColor.RED:
+                            rendererList[no].material = gMaterialsRed[level-2];
+                            break;
+                            // 青
+                        case GM_MathFlowerParam.EFlowerColor.BLUE:
+                            break;
+                            // 緑
+                        case GM_MathFlowerParam.EFlowerColor.GREEN:
+                            break;
+                    }
+                    //objectList[no].GetComponent<Renderer>().material = gMaterials[ (int)colorList[Id], level-1];
                     break;
             }
         }
