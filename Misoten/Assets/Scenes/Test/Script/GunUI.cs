e5f91c85769304e164e929c281b40f1e22da6089
Christoper0710 <g.gorillagorilla@gmail.com>
2016-11-18 10:09:06 +0900

    11/18 10:10 by commit

---
+++ Misoten/Assets/Scenes/Test/Script/GunUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunUI : MonoBehaviour {
+
+    public GameObject[] m_GunUI;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
