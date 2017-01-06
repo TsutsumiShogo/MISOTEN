fbf7a09f16b08b65385b843ad93230fa5f5ed35f
Christoper0710 <g.gorillagorilla@gmail.com>
2017-01-07 01:59:53 +0900

    タイトル雲修正、exe未確認

--- Misoten/Assets/Scripts/Title/LoopCloud.cs
+++ Misoten/Assets/Scripts/Title/LoopCloud.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class LoopCloud : MonoBehaviour {
 
+    public float m_time = 20.0f;
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(-0.1f, 0, 0);
-        if (transform.localPosition.x < -900.0f)
+        transform.localPosition = new Vector3(
+            transform.localPosition.x - 900.0f * Time.deltaTime / m_time,
+            transform.localPosition.y,
+            transform.localPosition.z);
+        if (transform.localPosition.x <= -900.0f)
         {
             transform.localPosition = new Vector3(900.0f, 0, 0);
         }
